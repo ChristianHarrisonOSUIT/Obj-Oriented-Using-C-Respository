f398fa68331cc4224208e717feb7986a05a16447
ChristianHarrisonOSUIT <126413616+ChristianHarrisonOSUIT@users.noreply.github.com>
2023-11-21 18:30:47 -0600

    Add files via upload

---
+++ ModulusOperation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber2
+{
+    public class ModulusOperation
+    {
+        public static string CalculateModulus(int dividend, int divisor)
+        {
+            int quotient = 0;
+            int remainder = dividend;
+
+            while (remainder >= divisor)
+            {
+                remainder -= divisor;
+                quotient++;
+            }
+
+            return $"{dividend} divided by {divisor} is {quotient} with a remainder of {remainder}";
+        }
+    }
+}
---
+++ Validation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNumber2
+{
+    public static class Validation
+    {
+        public static bool IsValidInteger(string input, int minValue, int maxValue)
+        {
+            if (int.TryParse(input, out int number))
+            {
+                return number >= minValue && number <= maxValue;
+            }
+            return false;
+        }
+    }
+}
